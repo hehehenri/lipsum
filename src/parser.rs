d12aa48ac9e12cb3c5328547961ad31baaa2c1d9
henri <henri1i.dev@gmail.com>
2023-09-11 04:40:00 -0300

    feat: use serde to parse json to ast

--- src/parser.rs
+++ src/parser.rs
@@ -1,10 +1,11 @@
-use crate::ast::Term;
+use crate::ast::File;
 
-struct ParseError {
+#[derive(Debug)]
+pub struct ParseError {
     pub message: String,
 }
 
-pub fn parse(json: String) -> Result<Term, ParseError> {
+pub fn parse(json: String) -> Result<File, ParseError> {
     serde_json::from_str(&json).map_err(|err| ParseError {
         message: err.to_string(),
     })
