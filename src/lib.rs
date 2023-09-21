f41a13605a589084065fcfa48d51dcc138db7deb
henri <henri1i.dev@gmail.com>
2023-09-21 14:58:23 -0300

    feat: parse from stdin

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,2 @@
-use ast::File;
-
 pub mod ast;
 pub mod interp;
-
-#[derive(Debug)]
-pub struct ParseError {
-    pub message: String,
-}
-
-pub fn parse(path: &str) -> Result<File, ParseError> {
-    let file_json = std::fs::read_to_string(path).unwrap();
-
-    serde_json::from_str(&file_json).map_err(|err| ParseError {
-        message: err.to_string(),
-    })
-}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-use rinha::{interp::eval_file, parse};
+use std::io;
+
+use rinha::interp::eval_file;
 
 fn main() {
-    let file_ast = parse("./files/fib.json").unwrap();
+    let mut file_buffer = String::new();
+    io::stdin().read_line(&mut file_buffer).unwrap();
+
+    let file_ast = serde_json::from_str(&file_buffer)
+        .map_err(|err| err.to_string())
+        .unwrap();
 
     let _ = eval_file(file_ast).unwrap();
 }
