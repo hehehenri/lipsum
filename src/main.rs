70ee456179cabbd624e68bd862a8ae7639e1b7a5
henri <henri1i.dev@gmail.com>
2023-09-25 02:31:16 -0300

    feat: read source from /var/rinha/source.rinha.json

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,21 @@ use rinha::interp::eval_file;
 #[derive(Parser, Debug)]
 struct Args {
     #[arg(short, long)]
-    file: String,
+    file: Option<String>,
 }
+
+static DEFAULT_PATH: &'static str = "/var/rinha/source.rinha.json";
 
 fn main() -> Result<(), String> {
     let args = Args::parse();
+    let path = match args.file {
+        Some(file) => file,
+        None => DEFAULT_PATH.to_string(),
+    };
 
-    let file_buffer = std::fs::read_to_string(args.file).map_err(|err| err.to_string())?;
+    let file_buffer = std::fs::read_to_string(path).expect("failed to load source");
 
-    let file_ast = serde_json::from_str(&file_buffer)
-        .map_err(|err| err.to_string())
-        .unwrap();
+    let file_ast = serde_json::from_str(&file_buffer).expect("failed to parse ast");
 
     let _ = eval_file(file_ast).unwrap();
 
